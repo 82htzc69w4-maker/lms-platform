3dcdb51e74b69afbb54bf474c4c3d16c6e3409dc
82htzc69w4-maker <82htzc69w4@privaterelay.appleid.com>
2026-09-15 12:35:32 +0200

    Remove temporary account recovery endpoints now that access has been restored

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ import productivityMetrics from './productivityMetrics/routes';
 import { employeePerformanceHtml } from './employee-performance';
 import performanceAppraisals from './performanceAppraisals/routes';
 import learningPlans from './learningPlans/routes';
-import tempRecovery from './tempRecovery/routes';
 import portfolioEvidence from './portfolioEvidence/routes';
 import workplaceObservations from './workplaceObservations/routes';
 
@@ -93,6 +92,5 @@ app.route('/api/portfolio-evidence', portfolioEvidence);
 app.route('/api/workplace-observations', workplaceObservations);
 app.route('/api/performance-appraisals', performanceAppraisals);
 app.route('/api/learning-plans', learningPlans);
-app.route('/api/temp-recovery', tempRecovery);
 
 export default app;
